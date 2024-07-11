3c6aae5f79c5591973f814e5d52f827b54fd4222
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-07-12 02:04:10 +0300

    Implemented generic API consumer with GET, POST, PUT, DELETE methods

---
+++ lib/core/networking/api_consumer.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+
+abstract class ApiConsumer {
+  Future<dynamic> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+    Options? options,
+  });
+
+  Future<dynamic> post(
+    String path, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+    Options? options,
+  });
+
+  Future<dynamic> put(
+    String path, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+    Options? options,
+  });
+
+  Future<dynamic> delete(
+    String path, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+    Options? options,
+  });
+}
