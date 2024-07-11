4114f4a32459b8b051e5a5598784242b7e1e909f
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-07-12 02:00:15 +0300

    Make main file clean

---
+++ lib/route_task.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RouteTask extends StatelessWidget {
+  const RouteTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Route Task',
+      theme: ThemeData(
+          // primaryColor: AppColors.primaryColor,
+          ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
