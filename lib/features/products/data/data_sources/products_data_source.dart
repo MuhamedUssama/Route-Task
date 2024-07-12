9fe5da52e602757b842bdf5afb14fc7415701833
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-07-12 03:04:47 +0300

    Created products repository & data source contract classes

---
+++ lib/features/products/data/data_sources/products_data_source.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:route_task/core/errors/exceptions.dart';
+import 'package:route_task/features/products/domain/models/products_dto.dart';
+
+abstract class ProductsDataSource {
+  Future<Either<ServerException, List<ProductsDto?>?>> getProducts();
+}
